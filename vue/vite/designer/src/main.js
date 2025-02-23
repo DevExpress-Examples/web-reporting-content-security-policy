71333ea808861cff3c05bffc7fe31c62364ea31b
Polina Tyureva <polina.tyureva@devexpress.com>
2025-02-23 17:37:23 +0400

    update vue part

---
+++ vue/vite/designer/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "ace-builds/css/ace.css";
+import "ace-builds/css/theme/dreamweaver.css";
+import "ace-builds/css/theme/ambiance.css"; 
+import "devextreme/dist/css/dx.light.css";
+import "@devexpress/analytics-core/dist/css/dx-analytics.common.css";
+import "@devexpress/analytics-core/dist/css/dx-analytics.light.css";
+import "@devexpress/analytics-core/dist/css/dx-querybuilder.css";
+import "devexpress-reporting/dist/css/dx-webdocumentviewer.css";
+import "devexpress-reporting/dist/css/dx-reportdesigner.css";
+
+createApp(App).mount('#app')
