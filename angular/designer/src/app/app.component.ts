c3e512346fcff31299736ca31a1eb5f7da5a8453
Polina Tyureva <polina.tyureva@devexpress.com>
2026-02-09 19:58:00 +0400

    update angular app

--- angular/designer/src/app/app.component.ts
+++ angular/designer/src/app/app.component.ts
@@ -1,29 +1,26 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { DxReportDesignerModule } from 'devexpress-reporting-angular';
 import 'devexpress-reporting/dx-richedit';
 
 @Component({
-  selector: 'app-root',
-  encapsulation: ViewEncapsulation.None,
-  standalone: true,
-  imports: [
-	RouterOutlet,
-	DxReportDesignerModule
-	],
-  templateUrl: './app.component.html',
-  styleUrls: [
-    "../../node_modules/ace-builds/css/ace.css",
-    "../../node_modules/ace-builds/css/theme/dreamweaver.css",
-    "../../node_modules/ace-builds/css/theme/ambiance.css",
-    "../../node_modules/devextreme/dist/css/dx.light.css",
-    "../../node_modules/devexpress-richedit/dist/dx.richedit.css",
-    "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css",
-    "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.light.css",
-    "../../node_modules/@devexpress/analytics-core/dist/css/dx-querybuilder.css",
-    "../../node_modules/devexpress-reporting/dist/css/dx-webdocumentviewer.css",
-    "../../node_modules/devexpress-reporting/dist/css/dx-reportdesigner.css"
-]
+    selector: 'app-root',
+    encapsulation: ViewEncapsulation.None,
+    imports: [
+        DxReportDesignerModule
+    ],
+    templateUrl: './app.component.html',
+    styleUrls: [
+        "../../node_modules/ace-builds/css/ace.css",
+        "../../node_modules/ace-builds/css/theme/dreamweaver.css",
+        "../../node_modules/ace-builds/css/theme/ambiance.css",
+        "../../node_modules/devextreme/dist/css/dx.light.css",
+        "../../node_modules/devexpress-richedit/dist/dx.richedit.css",
+        "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css",
+        "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.light.css",
+        "../../node_modules/@devexpress/analytics-core/dist/css/dx-querybuilder.css",
+        "../../node_modules/devexpress-reporting/dist/css/dx-webdocumentviewer.css",
+        "../../node_modules/devexpress-reporting/dist/css/dx-reportdesigner.css"
+    ]
 })
 export class AppComponent {
     title = 'DXReportDesignerSample';
--- angular/viewer/src/app/app.component.ts
+++ angular/viewer/src/app/app.component.ts
@@ -1,22 +1,19 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { DxReportViewerModule } from 'devexpress-reporting-angular';
 
 @Component({
-  selector: 'app-root',
-  encapsulation: ViewEncapsulation.None,
-  standalone: true,
-  imports: [
-	RouterOutlet,
-	DxReportViewerModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrls: [
-    "../../node_modules/devextreme/dist/css/dx.light.css",
-    "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css",
-    "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.light.css",
-    "../../node_modules/devexpress-reporting/dist/css/dx-webdocumentviewer.css"
-]
+    selector: 'app-root',
+    encapsulation: ViewEncapsulation.None,
+    imports: [
+        DxReportViewerModule
+    ],
+    templateUrl: './app.component.html',
+    styleUrls: [
+        "../../node_modules/devextreme/dist/css/dx.light.css",
+        "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.common.css",
+        "../../node_modules/@devexpress/analytics-core/dist/css/dx-analytics.light.css",
+        "../../node_modules/devexpress-reporting/dist/css/dx-webdocumentviewer.css"
+    ]
 })
 export class AppComponent {
 	title = 'DXReportViewerSample';
